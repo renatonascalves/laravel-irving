cf072fc7601d00b48a4d12edc79003c17b8719ba
Renato Alves <espellcaste@gmail.com>
2020-03-27 18:09:51 -0300

    Return early if path already has forward slash

--- src/Middleware/ForceTrailingSlashes.php
+++ src/Middleware/ForceTrailingSlashes.php
@@ -4,14 +4,15 @@ namespace Irving\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ForceTrailingSlashes
 {
 	/**
 	* Handle an incoming request.
 	*
-	* @param  \Illuminate\Http\Request  $request
-	* @param  \Closure  $next
+	* @param  \Illuminate\Http\Request $request
+	* @param  \Closure                 $next
 	* @return mixed
 	*/
 	public function handle(Request $request, Closure $next)
@@ -23,10 +24,16 @@ class ForceTrailingSlashes
 		}
 
 		$params = $request->all();
+		$path   = $params['path'] ?? '';
 		$uri    = $request->getRequestUri();
 
+		// Return if there is already a trailing slash.
+		if ($path === '/' || Str::endsWith($path, '/')){
+			return $next($request);
+		}
+
 		// Redirect to ?path=/ (home)
-		if (empty($params['path'])) {
+		if (empty($path)) {
 			return \redirect()->to( $appUrl . $uri . '?path=/' );
 		} else {
 
